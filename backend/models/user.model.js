86d0b02b21a8f76774667b59a277a85c0a31fa27
Larrysmart101 <olanrewajufolami21@gmail.com>
2024-05-17 12:50:18 +0100

    twitter

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+ 
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    fullName:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLenght: 6,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    followers:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            default: []
+        }
+    ],
+    following:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            default: []
+        }
+    ],
+    profileImg:[
+        {
+            type:String,
+            ref:"Image",
+            default: []
+        }
+    ],
+},{timestamps:true});
